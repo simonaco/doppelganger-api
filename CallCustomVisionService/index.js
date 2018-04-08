5c2bbac32c030ca51a156c97824eafb2207339bd
Simona Cotin <simona.cotin@gmail.com>
2018-04-08 11:10:26 +0100

    implement call to CVS

--- CallCustomVisionService/index.js
+++ CallCustomVisionService/index.js
@@ -1,9 +1,26 @@
+const request = require('request');
+const API_URL =
+  'https://southcentralus.api.cognitive.microsoft.com/customvision/v1.1/Prediction/bbfb33b7-4e47-482c-b7b4-5a1a92f66180/image?iterationId=0d4497ac-d6b5-4987-8c66-b03c5358c59e';
 module.exports = function(context, req) {
   context.log('JavaScript HTTP trigger function processed a request.');
 
-  context.res = {
-    // status: 200, /* Defaults to 200 */
-    body: 'Hello ' + (req.query.name || req.body.name)
+  const options = {
+    url: API_URL,
+    headers: {
+      'Content-Type': 'application/octet-stream',
+      'Prediction-Key': process.env.ACCESS_KEY
+    },
+    method: 'POST',
+    body: req.body
   };
-  context.done();
+  request(options, (err, result) => {
+    if (err) {
+      context.log('Error' + err);
+      context.done();
+      return;
+    } else {
+      context.log(result.body);
+      context.done();
+    }
+  });
 };
